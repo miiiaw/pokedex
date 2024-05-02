560a242f9f4c46d3d2a2c189547e43ca8bfb21fa
miiiaw <141754937+miiiaw@users.noreply.github.com>
2024-05-02 23:00:47 +0200

    Good night
    Finally remembered the fucking ?, and could write it a loootlot more simple. Heck.

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -4,51 +4,57 @@ import { useParams } from "react-router-dom"
 export default function Pokemon({ pokemon }) {
     // Storing the slug in a variable and logging it
     const {pokemonId} = useParams()
-
-    // Array for Ability facts
-    const [abilityDetails, setAbilityDetails] = useState([])
-
     // Filtered array of the Pokemon
     const [pokemonDetails, setPokemonDetails] = useState([])
+    // Variabel for URL for new API to fetch the ability details
+    let aURL = []
+    // Array for Ability details
+    const [abilityDetails, setAbilityDetails] = useState([])
+    
 
     // Filtering through the Pokemon-array and store the specified Pokemon in PokemonDetails.
     useEffect(() => {
-        const filteredPokemon = pokemon.filter(pokemon => Number(pokemon.id) === Number(pokemonId))
-        setPokemonDetails(filteredPokemon)
+        setPokemonDetails(pokemon?.filter(pokemon => Number(pokemon.id) === Number(pokemonId)))
     }, [pokemonId, pokemon])
+
+    pokemonDetails[0]?.abilities.map(abilities => (
+        aURL.push(abilities.ability.url)
+    ))
+
 
     // New API call to fetch ability details
     useEffect(() => {
-        const fetchAbilities = async () => {
-            const abilities = []
+        const getAbilityDetails = async () => {
+          try {
+            const results = await Promise.all(aURL.map(url =>
+              fetch(url)
+              .then(response => 
+                response.json())
+            )
+            )
+            setAbilityDetails(results)
+          } catch (error) {
+            console.error("Nope. Not today.", error)
+          }
+        };
     
-            for (const pokemon of pokemonDetails) {
-                for (const ability of pokemon.abilities) {
-                    const response = await fetch(ability.ability.url)
-                    const data = await response.json()
-                    abilities.push(data)
-                }
-            }
-    
-            setAbilityDetails(abilities)
-        }
-            fetchAbilities()
-    }, [pokemonDetails])
-    
+        getAbilityDetails()
+      }, [pokemonDetails])
+
+      console.log(abilityDetails)
 
     return (
         <>
-        {pokemonDetails.map(pokemon => (
-            <h1 key={pokemon.id}>{pokemon.name}</h1>
-        ))}
+        <h1>{pokemonDetails[0]?.name}</h1>
 
         {abilityDetails.map((ability, index) => (
             <article key={index}>
-                <h2>{ability.name}</h2>
+                <h2>{ability?.name}</h2>
+                <p>{ability?.effect_entries[0].effect}</p>
 
 
 
-                
+
             </article>
         ))}
         </>
