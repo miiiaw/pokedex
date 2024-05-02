9af15ef54e5f3a80dcf040a4c8e565e41130b21f
miiiaw <141754937+miiiaw@users.noreply.github.com>
2024-05-02 20:27:15 +0200

    for() loop to the rescue
    something is finally working. do not touch.

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -4,57 +4,53 @@ import { useParams } from "react-router-dom"
 export default function Pokemon({ pokemon }) {
     // Storing the slug in a variable and logging it
     const {pokemonId} = useParams()
-    console.log(pokemonId)
+
     // Array for Ability facts
-    const [abilityNames, setAbilityNames] = useState([])
-    // Array for Ability facts
-    const [abilityFacts, setAbilityFacts] = useState([])
+    const [abilityDetails, setAbilityDetails] = useState([])
+
     // Filtered array of the Pokemon
     const [pokemonDetails, setPokemonDetails] = useState([])
+
     // Filtering through the Pokemon-array and store the specified Pokemon in PokemonDetails.
     useEffect(() => {
         const filteredPokemon = pokemon.filter(pokemon => Number(pokemon.id) === Number(pokemonId))
         setPokemonDetails(filteredPokemon)
     }, [pokemonId, pokemon])
 
-    // Nytt API kall for å hente informasjon om abilities
+    // New API call to fetch ability details
     useEffect(() => {
-        pokemonDetails.map(pokemon => {
-            pokemon.abilities.map(async (ability) => {
-                const response = await fetch(ability.ability.url)
-                const data = await response.json()
-                setAbilityNames(data)
-                setAbilityFacts(data.effect_entries)
-            })
-        })
+        const fetchAbilities = async () => {
+            const abilities = []
+    
+            for (const pokemon of pokemonDetails) {
+                for (const ability of pokemon.abilities) {
+                    const response = await fetch(ability.ability.url)
+                    const data = await response.json()
+                    abilities.push(data)
+                }
+            }
+    
+            setAbilityDetails(abilities)
+        }
+            fetchAbilities()
     }, [pokemonDetails])
-
-    console.log(abilityFacts)
-    console.log(abilityNames)
+    
 
     return (
         <>
         {pokemonDetails.map(pokemon => (
-        <article key={pokemon.id}>
-            <h1>{pokemon.name}</h1>
-
-            {pokemon.abilities.map((ability, i) => (
-            <h3 key={i}>
-                {ability.ability.name}
-            </h3>
-            
-
-            ))}
-        </article>
+            <h1 key={pokemon.id}>{pokemon.name}</h1>
         ))}
-        
-        {abilityFacts.map((ability, index) => (
-            <article key={index}>
-                <p>Effect: {ability.effect}</p>
-                <p>Short effect: {ability.short_effect}</p>
-            </article>
-            ))}
 
+        {abilityDetails.map((ability, index) => (
+            <article key={index}>
+                <h2>{ability.name}</h2>
+
+
+
+                
+            </article>
+        ))}
         </>
     )
 }
